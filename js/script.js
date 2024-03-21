98d5fa56402656faf0b9bc96e9bae45f563ad451
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-03-21 18:48:21 +0000

    save all

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 hames.al-sharoa All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by:hames.al-sharoa
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculate () {
+  // input
+  const length = parseInt(document.getElementById('length-of-rectangle').value)
+  const width = parseInt(document.getElementById('width-of-rectangle').value)
+
+  // process
+  const area = length * width
+  const perimeter = 2 * (length + width)
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm'
+}
